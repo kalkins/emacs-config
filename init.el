9ad6c0c664a9299d3148ed33b3ee2f924122294d
Sindre Stephansen <sindre@sindrestephansen.com>
2022-04-14 02:04:39 +0200

    Rewrite to be used as the emacs directory, to support chemacs

--- init.el
+++ init.el
@@ -1,9 +1,5 @@
 ;;;; This script sets up emacs for a config file
 ;;;; written in org mode.
-;;;;
-;;;; It should only be executed once, when the config is first used.
-;;;; You should restart emacs afterwards, to make sure that all
-;;;; packages get loaded properly.
 
 ;;; Code:
 
@@ -11,8 +7,6 @@
 ;; by the config, unless it has been installed already
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
@@ -22,22 +16,7 @@
   (setq config-dir (file-name-directory
 		    (or load-file-name buffer-file-name)))
 
-  ;; Ask the user if they want to use the default
-  ;; config, or provide the path to another one
-  (setq config-path (read-file-name
-		     "Path to config file: "
-		     config-dir
-		     nil
-		     t
-		     "config.org"))
-
-  ;; Append a line to .emacs which loads the
-  ;; config file at startup
-  (write-region
-   (concat "(org-babel-load-file \"" config-path "\")")
-   nil
-   "~/.emacs"
-   'append)
+  (setq config-path (concat config-dir "config.org"))
 
   ;; Load the config, and install all the packages
   (org-babel-load-file config-path))
